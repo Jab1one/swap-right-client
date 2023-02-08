901847139c1d4de791be4b147c30ca4cb5632b6c
Julien Abraham <julienabraham@gmail.com>
2023-02-08 10:10:50 -0500

    create a list item page with working upload features

---
+++ src/pages/ListItem/ListItem.jsx
@@ -0,0 +1,92 @@
+import "./ListItem.scss";
+import React, { useState } from "react";
+import axios from "axios";
+
+const serverUrl = process.env.SERVER_URL;
+
+const ListItem = () => {
+  const [images, setImages] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleImageUpload = (event) => {
+    setImages(
+      Array.from(event.target.files).map((file) => URL.createObjectURL(file))
+    );
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("description", description);
+    images.forEach((image) => formData.append("images", image));
+
+    try {
+      const response = await axios.post({ serverUrl }, formData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="submit-form-container">
+      <form action="submit" className="submit-form">
+        <div className="submit-form__image-container">
+          <div className="image-container">
+            {images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt="upload preview"
+                className="image-upload-preview"
+              />
+            ))}
+          </div>
+          <label className="image-upload-label">
+            Add Images
+            <input
+              type="file"
+              onChange={handleImageUpload}
+              multiple
+              className="image-upload-input"
+            />
+          </label>
+        </div>
+
+        <p className="why-text">
+          Swap Right makes uploading items simple. Select the item's images
+          using the Add image button, then enter its details such as title and
+          description. Finally, press the upload button to share your item with
+          the Swap Right community.
+        </p>
+
+        <input
+          type="text"
+          className="submit-form__item-title"
+          id="title"
+          placeholder="Listing title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          required
+        ></input>
+
+        <textarea
+          type="text-area"
+          className="submit-form__item-description"
+          id="email"
+          placeholder="Item description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+          required
+        ></textarea>
+
+        <button type="submit" className="submit-form__button">
+          Upload
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ListItem;
